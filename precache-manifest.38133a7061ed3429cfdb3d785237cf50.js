0e2df0badf5d366a0e881e3ef4eab59068e5e7e4
Lavi <lavisharma796@gmail.com>
2019-01-12 07:46:36 +0000

    Updates

---
+++ precache-manifest.38133a7061ed3429cfdb3d785237cf50.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c49ea506aaacbe477fc",
+    "url": "/React-Redx-Simple/static/js/runtime~main.7c49ea50.js"
+  },
+  {
+    "revision": "5c9ea22eb9fa62830407",
+    "url": "/React-Redx-Simple/static/js/main.5c9ea22e.chunk.js"
+  },
+  {
+    "revision": "eb5a6a5a6336a6e05669",
+    "url": "/React-Redx-Simple/static/js/1.eb5a6a5a.chunk.js"
+  },
+  {
+    "revision": "cb8cf88bf75904ec4a88a47ab2041060",
+    "url": "/React-Redx-Simple/index.html"
+  }
+];
